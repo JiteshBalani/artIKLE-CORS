854b4f9903cb0da8e2ae51e93823e54122446d68
JiteshBalani <balani.jitesh911@gmail.com>
2024-03-05 20:31:20 +0530

    Made the backend for artIKLE project

---
+++ env.js
@@ -0,0 +1,1 @@
+export const newsData = '7409a41708f44b0babf13a6aff79dc94';
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import fetch from "cross-fetch";
+import { newsData } from "./env.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.get('/newsData', (req, res) => {
+    const url = `https://newsapi.org/v2/everything?q=keyword&apiKey=${newsData}`;
+  
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('An error occurred');
+      });
+  });
+
+
+app.listen(port, ()  => console.log(`Server started on port ${port}`));
